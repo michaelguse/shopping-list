252c2dc4280fb0b0a58d0cc44466d21362b7c6f3
Michael Guse <guse.michael@gmail.com>
2013-03-18 15:45:55 -0500

    Added Rails project default folders.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ShoppingList::Application.config.secret_token = 'bc6733c6823e0423f9be57c715fb23b54243fac81dd434a26e05671664f41f233796db3d52d082011c1416ca29457517d679775dbf48c4613e73fc44f07044b2'
